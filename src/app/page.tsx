5b05779c8b6f82e2f00fdce0a3de95573cf62922
liyongqing <1906813941@qq.com>
2024-10-08 21:16:41 +0800

    前端代码初始化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <button type="primary">Primary Button</button>
         <Image
           className={styles.logo}
           src="https://nextjs.org/icons/next.svg"
